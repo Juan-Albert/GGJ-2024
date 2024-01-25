1159dd348f09c91570dc309fd0fb0b31f5875c28
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2024-01-25 22:09:58 +0100

    feat: pass beat frames

--- Assets/Runtime/Domain/Beat.cs
+++ Assets/Runtime/Domain/Beat.cs
@@ -1,6 +1,28 @@
-﻿namespace Runtime.Domain
+﻿using System;
+
+namespace Runtime.Domain
 {
     public class Beat
     {
+        private int framesDuration;
+
+        public int CurrentFrame { get; private set; }
+
+        public Beat(int framesDuration)
+        {
+            if (framesDuration <= 0)
+                throw new NotSupportedException("No se puede crear un beat con 0 o menos frames");
+            
+            this.framesDuration = framesDuration;
+            CurrentFrame = 0;
+        }
+
+        public void Next()
+        {
+            if (CurrentFrame >= framesDuration)
+                throw new NotSupportedException("El beat ya habia terminado y quería seguir");
+
+            CurrentFrame += 1;
+        }
     }
 }
---
+++ Assets/Runtime/Tests/EditMode/BeatTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Runtime.Domain;
+
+namespace Runtime.Tests.EditMode
+{
+    public class BeatTests
+    {
+        [Test]
+        public void BeatStartAt_FirstFrame()
+        {
+            Assert.True(new Beat(1).CurrentFrame == 0);
+        }
+
+        [Test]
+        public void BeatNextFrame()
+        {
+            var sut = new Beat(2);
+            sut.Next();
+            Assert.True(sut.CurrentFrame == 1);
+            sut.Next();
+            Assert.True(sut.CurrentFrame == 2);
+
+        }
+    }
+}
