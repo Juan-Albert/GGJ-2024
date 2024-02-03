45289f40fe8dadbd8b781d51cff9b1fd874c6bf7
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2024-02-03 23:24:52 +0100

    feat: calculate seconds per beat

---
+++ Assets/Runtime/Domain/Tempo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Runtime.Domain
+{
+    public class Tempo
+    {
+        public Tempo(int beatsPerMinute)
+        {
+            if (beatsPerMinute <= 0)
+                throw new NotSupportedException("el tempo no puede ser igual o menor a cero");
+            
+            BeatsPerMinute = beatsPerMinute;
+        }
+
+        public int BeatsPerMinute { get; }
+        
+        public float SecondsPerBeat => 60f / BeatsPerMinute;
+
+        public static Tempo Lento => new(40);
+        public static Tempo Moderato => new(80);
+        public static Tempo Presto => new(160);
+
+    }
+}
---
+++ Assets/Runtime/Tests/EditMode/TempoTest.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Runtime.Domain;
+
+namespace Runtime.Tests.EditMode
+{
+    public class TempoTest
+    {
+        [Test]
+        public void SecondsPerBeat()
+        {
+            Tempo.Lento.SecondsPerBeat.Should().BeApproximately(2f, 0.001f);
+            Tempo.Moderato.SecondsPerBeat.Should().BeApproximately(0.75f, 0.001f);
+            Tempo.Presto.SecondsPerBeat.Should().BeApproximately(0.375f, 0.001f);
+        }
+    }
+}
