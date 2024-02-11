b374f636a9e1f9cd256d92d13926887ae9546de8
Juan Albert <juanmartinezlopez96@gmail.com>
2024-02-11 19:54:20 +0100

    feat: create clown game notes

--- Assets/Runtime/Domain/Note.cs
+++ Assets/Runtime/Domain/Note.cs
@@ -10,8 +10,13 @@
         }
 
         public static Note Silence => new ("Silence");
+        public static Note Ball => new ("Ball");
+        public static Note Juggle => new ("Juggle");
+        public static Note Handstand => new ("Handstand");
+        public static Note Trumpet => new ("Trumpet");
 
         public static implicit operator string(Note note) => note.Sound;
         public static implicit operator Note(string sound) => new (sound);
+        
     }
 }
