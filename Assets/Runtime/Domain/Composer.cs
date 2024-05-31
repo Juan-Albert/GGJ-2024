9c2a249c578ab73949df6bdfc56975405ab94ff5
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2024-05-31 18:03:34 +0200

    feat: compose random music

--- Assets/Runtime/Domain/Composer.cs
+++ Assets/Runtime/Domain/Composer.cs
@@ -1,34 +1,44 @@
-﻿namespace Runtime.Domain
+﻿using System;
+using Random = UnityEngine.Random;
+
+namespace Runtime.Domain
 {
     public static class Composer
     {
+        private static Sheet[] sheetBook = 
+        {
+            new(Tempo.Prestissimo, new ForwardTime(), new[]
+            {
+                new Beat(1, Note.Handstand),
+                new Beat(1, Note.Handstand),
+                new Beat(1, Note.Handstand),
+                new Beat(1, Note.Handstand),
+                new Beat(1, Note.Handstand),
+                new Beat(1, Note.Handstand),
+                new Beat(1, Note.Handstand),
+                new Beat(1, Note.Handstand),
+            })
+        };
+        
         public static Song Compose()
         {
-            return new Song
-            (new Sheet(Tempo.Prestissimo, new ForwardTime(), new[]
-                {
-                    new Beat(1, Note.Handstand),
-                    new Beat(1, Note.Handstand),
-                    new Beat(1, Note.Handstand),
-                    new Beat(1, Note.Handstand),
-                    new Beat(1, Note.Handstand),
-                    new Beat(1, Note.Handstand),
-                    new Beat(1, Note.Handstand),
-                    new Beat(1, Note.Handstand),
-                }),
-                new Sheet(Tempo.Prestissimo, new ForwardTime(), new[]
-                {
-                    new Beat(1, Note.Handstand),
-                    new Beat(1, Note.Handstand),
-                    new Beat(1, Note.Handstand),
-                    new Beat(1, Note.Handstand),
-                    new Beat(1, Note.Handstand),
-                    new Beat(1, Note.Handstand),
-                    new Beat(1, Note.Handstand),
-                    new Beat(1, Note.Handstand),
-                })
-            );
+            return new Song(RandomMusic(), Tempo.Prestissimo.AsRhythm());
         }
+
+        private static Sheet RandomMusic() => sheetBook[Random.Range(0, sheetBook.Length)];
+
+        private static Sheet AsRhythm(this Tempo tempo) 
+            => new(tempo, new ForwardTime(), new[]
+                {
+                    new Beat(1, Note.Handstand),
+                    new Beat(1, Note.Handstand),
+                    new Beat(1, Note.Handstand),
+                    new Beat(1, Note.Handstand),
+                    new Beat(1, Note.Handstand),
+                    new Beat(1, Note.Handstand),
+                    new Beat(1, Note.Handstand),
+                    new Beat(1, Note.Handstand),
+                });
     }
 
 }
