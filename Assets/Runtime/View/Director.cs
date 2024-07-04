6317b9518ed5f6006e3fd46eb7a4b9ab0fbfc569
Sara P.L <57196305+saraplantes@users.noreply.github.com>
2024-07-04 21:21:59 +0200

    content: changes in director script to add arrows linked to director clown poses and changes in main scene to assign the arrows sprites, change of unity version

--- Assets/Runtime/View/Director.cs
+++ Assets/Runtime/View/Director.cs
@@ -6,12 +6,19 @@ using UnityEngine;
 public class Director : MonoBehaviour, MusicianOutput
 {
     [SerializeField] private SpriteRenderer clownRenderer;
-    
+    [SerializeField] private SpriteRenderer arrowRenderer;
+
+    [Header("Clown poses")]
     [SerializeField] private Sprite ballSprite;
     [SerializeField] private Sprite handstandSprite;
     [SerializeField] private Sprite juggleSprite;
     [SerializeField] private Sprite trumpetSprite;
     [SerializeField] private Sprite idleSprite;
+    [Header("Key inputs")]
+    [SerializeField] private Sprite upSprite;
+    [SerializeField] private Sprite downSprite;
+    [SerializeField] private Sprite leftSprite;
+    [SerializeField] private Sprite rightSprite;
 
     private Tempo _tempo;
 
@@ -23,15 +30,30 @@ public class Director : MonoBehaviour, MusicianOutput
         void PrintClownSprite()
         {
             if (note.Equals(Note.Silence))
+            {
                 clownRenderer.sprite = idleSprite;
+                arrowRenderer.sprite = null;
+            }
             else if (note.Equals(Note.Ball))
+            {
                 clownRenderer.sprite = ballSprite;
+                arrowRenderer.sprite = upSprite;
+            }
             else if (note.Equals(Note.Handstand))
+            {
                 clownRenderer.sprite = handstandSprite;
+                arrowRenderer.sprite = downSprite;
+            }
             else if (note.Equals(Note.Juggle))
+            {
                 clownRenderer.sprite = juggleSprite;
+                arrowRenderer.sprite = rightSprite;
+            }
             else if (note.Equals(Note.Trumpet))
+            {
                 clownRenderer.sprite = trumpetSprite;
+                arrowRenderer.sprite = leftSprite;
+            }
             else
                 throw new NotSupportedException("No existe el sprite de esa nota");
         }
